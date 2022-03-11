03f4f7c019916de9c1336d94a1f1407e82d8548c
Tobias Goetz <tobigoetz98@gmail.com>
2022-03-11 16:17:02 +0100

    Added comments to navbar script

--- views/public/navbar.js
+++ views/public/navbar.js
@@ -1,18 +1,22 @@
-const body = document.querySelector("body"),
-    sidebar = body.querySelector(".sidebar"),
-    toggle = body.querySelector(".toggle"),
-    searchBtn = body.querySelector(".search-box"),
-    modeSwitch = body.querySelector(".toggle-switch"),
-    modeText = body.querySelector(".mode-text");
+// Defining and getting object to later modify
+const body = document.querySelector("body");
+const sidebar = body.querySelector(".sidebar");
+const toggle = body.querySelector(".toggle");
+const searchBtn = body.querySelector(".search-box");
+const modeSwitch = body.querySelector(".toggle-switch");
+const modeText = body.querySelector(".mode-text");
 
+// Add click event to open/close button of sidebar to open/close sidebar
 toggle.addEventListener("click", () => {
     sidebar.classList.toggle("close");
 });
 
+// Add click event to search button to expand(open) sidebar
 searchBtn.addEventListener("click", () => {
     sidebar.classList.remove("close");
 });
 
+// Add click event to mode switch to switch between light and dark mode
 modeSwitch.addEventListener("click", () => {
     body.classList.toggle("dark");
 
